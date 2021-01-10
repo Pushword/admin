a09211eec9ce40658562b3a6ae54476703afe6f5
RobinDev <contact@piedweb.com>
2021-01-10 19:22:12 +0000

    first functionnal test

---
+++ tests/AdminTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Pushword\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class AdminTest extends WebTestCase
+{
+    private bool $userCreated = false;
+
+    public function testLogin()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/admin/');
+        $this->assertEquals(301, $client->getResponse()->getStatusCode());
+
+        $client->request('GET', '/login');
+        $this->assertStringContainsString('Connexion', $client->getResponse());
+    }
+
+    public function loginUser(): KernelBrowser
+    {
+        if (false === $this->userCreated) {
+            $this->createUser();
+        }
+
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+        $form = $crawler->filter('[method=post]')->form();
+        $form['email'] = 'admin@example.tld';
+        $form['password'] = 'mySecr3tpAssword';
+        $crawler = $client->submit($form);
+
+        return $client;
+    }
+
+    public function testPageList()
+    {
+        $client = $this->loginUser();
+
+        $crawler = $client->request('GET', '/admin/app/page/list');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    private function createUser()
+    {
+        $kernel = static::createKernel();
+        $application = new Application($kernel);
+
+        $command = $application->find('pushword:user:create');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'email' => 'admin@example.tld',
+            'password' => 'mySecr3tpAssword',
+            'role' => 'ROLE_SUPER_ADMIN',
+        ]);
+    }
+}
