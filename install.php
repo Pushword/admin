a30389146aedd13d4f36381668cd1316d68942eb
RobinDev <contact@piedweb.com>
2024-04-03 08:30:46 +0000

    prepare v1

--- install.php
+++ install.php
@@ -1,17 +1,17 @@
 <?php
 
+use Pushword\Installer\PostInstall;
+
 /**
  * Execute via Pushword\Installer\PostInstall::postUpdateCommand.
  */
-
-
-if (! \Pushword\Installer\PostInstall::isRoot()) {
+if (! PostInstall::isRoot()) {
     throw new Exception('installer mus be run from root');
 }
 
-\Pushword\Installer\PostInstall::remove([
+PostInstall::remove([
     'config/packages/sonata_admin.yaml',
 ]);
 
 echo '~~ Adding Puswhord Admin Routes'.chr(10);
-\Pushword\Installer\PostInstall::addOnTop('config/routes.yaml', "admin:\n    resource: '@PushwordAdminBundle/AdminRoutes.yaml'\n");
+PostInstall::addOnTop('config/routes.yaml', "admin:\n    resource: '@PushwordAdminBundle/AdminRoutes.yaml'\n");
